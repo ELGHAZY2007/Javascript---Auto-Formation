4f55ec3194f77a4252fcf0a7040ae0918597d7dd
ELGHAZY2007 <elghazy.mohamedreda@gmail.com>
2022-01-17 10:52:42 +0100

    UP

---
+++ POO/oopPerson.js
@@ -0,0 +1,12 @@
+var person1 = new Person();
+
+person1._name = "EL GHAZY",
+
+person1.age = 19,
+
+person1.gender = "male",
+
+
+person1.display()
+
+console.log(person1._name, person1.age, person1._gender)
